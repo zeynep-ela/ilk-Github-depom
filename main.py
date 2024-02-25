88614161947caf2bfceaa63eecd4dd23013abcde
zeynep-ela <161217618+zeynep-ela@users.noreply.github.com>
2024-02-25 16:37:53 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+meme_dict = {
+            "CRINGE":"Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            }
+            
+
+
+print(meme.dict["CRINGE"])
+for i in meme_dict.keys():
+    print(i)
